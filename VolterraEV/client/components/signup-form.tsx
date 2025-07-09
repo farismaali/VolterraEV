ec70451106c4c107c04ab112ebe180eb3a03f378
David Crimi <crimid01@gmail.com>
2025-07-09 19:15:13 -0400

    configured login and register page ui

---
+++ VolterraEV/client/components/signup-form.tsx
@@ -0,0 +1,44 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+export function SignupForm({
+  className,
+  ...props
+}: React.ComponentProps<"form">) {
+  return (
+    <form className={cn("flex flex-col gap-6", className)} {...props}>
+      <div className="flex flex-col items-center gap-2 text-center">
+        <h1 className="text-2xl font-bold">Register</h1>
+      </div>
+      <div className="grid gap-6">
+        <div className="grid gap-3">
+          <Label htmlFor="email">Username</Label>
+          <Input id="email" type="email" placeholder="username123" required />
+        </div>
+        <div className="grid gap-3">
+          <div className="flex items-center">
+            <Label htmlFor="password">Password</Label>
+          </div>
+          <Input id="password" type="password" required />
+          <div className="flex items-center">
+            <Label htmlFor="confirmPassword">Confirm Password</Label>
+          </div>
+          <Input id="confirmPassword" type="confirmPassword" required />
+        </div>
+        <Button type="submit" className="w-full">
+          Sign up
+        </Button>
+        <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+        </div>
+      </div>
+      <div className="text-center text-sm">
+        Already have an account?{" "}
+        <a href="/auth/login" className="underline underline-offset-4">
+          Login
+        </a>
+      </div>
+    </form>
+  )
+}
